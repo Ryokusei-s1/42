93d3c64780ccb1b3f36152d3bb74a882c8eff778
Alberto Almendras Fuentealba <albalmen@c3r4s2.42urduliz.com>
2025-05-15 13:10:24 +0200

    falta pocoooo

--- arreglao/ft_memchr.c
+++ arreglao/ft_memchr.c
@@ -1,12 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albalmen <albalmen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/15 12:54:53 by albalmen          #+#    #+#             */
-/*   Updated: 2025/05/15 12:54:54 by albalmen         ###   ########.fr       */
+/*   Created: 2025/05/15 10:27:51 by albalmen          #+#    #+#             */
+/*   Updated: 2025/05/15 12:38:38 by albalmen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*ptr;
+	size_t				i;
+
+	i = 0;
+	ptr = (const unsigned char *)s;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char)c)
+			return ((void *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
